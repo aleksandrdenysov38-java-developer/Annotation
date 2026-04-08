02dc2b8ae17649a43f8b682e34c57ee245c25549
alex <aleksandrdenysov38@gmail.com>
2026-04-08 23:30:40 +0300

    My project

--- src/app/Author.java
+++ src/app/Author.java
@@ -2,8 +2,11 @@ package app;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface Author {
     String firstName();
     String lastName();
